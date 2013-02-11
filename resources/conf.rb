d74263355d932b235f56c3e64d01c0a0f5efa26b
Lloyd Philbrook <lloyd@firebellydesign.com>
2013-02-11 19:33:14 +0800

    added basic alert notification to resource

--- resources/conf.rb
+++ resources/conf.rb
@@ -9,6 +9,7 @@ attribute :name, :name_attribute => true, :kind_of => String
 attribute :path, :kind_of => String
 attribute :pid, :kind_of => String
 attribute :reload, :default => :delayed
+attribute :alert, :kind_of => String
 attribute :rule, :kind_of => [String, Array] #TODO: regex check for if then
 attribute :start, :kind_of => String
 attribute :start_as, :kind_of => String
