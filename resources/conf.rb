912d22d526b5d1f4ef5533c06d1da094effa5803
Gregoire Seux <g.seux@criteo.com>
2013-10-14 14:07:25 +0200

    Allow filesystem type

--- resources/conf.rb
+++ resources/conf.rb
@@ -16,7 +16,7 @@ attribute :start_as, :kind_of => String
 attribute :stop, :kind_of => String
 attribute :stop_as, :kind_of => String
 attribute :template, :kind_of => String, :default => "conf.erb"
-attribute :type, :equal_to => [:file, :process], :default => :process
+attribute :type, :equal_to => [:file, :process, :filesystem], :default => :process
 
 def initialize(*args)
   super
